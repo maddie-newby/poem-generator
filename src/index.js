fdfc44c52817186c6f9c4a0c77c7c70b9035c484
maddie-newby <madison1999@hotmail.co.uk>
2024-11-05 17:31:55 +0000

    Add colour to output

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function generatePoem(event) {
   let apiKey = "d84fo7b1165495bfa04e4513f7c437tf";
   let prompt = `Generate a poem about ${instructionsInput.value}`;
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include the HTML notation nor a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include the HTML notation nor a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element the color rgb(102 191 191) at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
