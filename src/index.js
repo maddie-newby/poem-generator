c5e0dd2f90539c84c46f29c85df31ddcea72c818
maddie-newby <madison1999@hotmail.co.uk>
2024-11-06 17:13:34 +0000

    Add hint, loading spinner and hide poem element

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 function displayPoem(response) {
+  let loaderElement = document.querySelector("#loader");
+  loaderElement.classList.add("hidden");
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -19,6 +25,13 @@ function generatePoem(event) {
   let context =
     "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include the HTML notation nor a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element the color rgb(102 191 191) at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let loaderElement = document.querySelector("#loader");
+  loaderElement.classList.remove("hidden");
+  loaderElement.innterHTML = `<span class="loader"></span>`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.add("hidden");
 
   axios.get(apiUrl).then(displayPoem);
 }
